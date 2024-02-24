83beabb184516407c6e46a472d29a1ddc3cc9f83
Fucloud-arch <270928474@qq.com>
2024-02-24 16:26:32 +0800

    feat: global component to use svg icon

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- 通过变量设置属性 -->
+    <Icon :style="{ width: width, fill: color }"></Icon>
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    width: {
+        type: Number,
+        default: 32,
+    },
+    color: {
+        type: String,
+        default: "black",
+    },
+});
+
+const Icon = defineAsyncComponent(
+    () => import(`../assets/icons/${props.name}.svg`)
+);
+</script>
