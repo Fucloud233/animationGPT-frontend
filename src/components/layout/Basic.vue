3e12e9f2bcf01d2bbafd235f6106b35a63af21e7
Fucloud-arch <270928474@qq.com>
2024-02-24 16:26:58 +0800

    feat: Basic Demo Page

---
+++ src/components/layout/Basic.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-container style="height: 100%">
+        <el-header>
+            <a href="/" id="title">AnimationGPT</a>
+
+            <!-- <embed src="../../assets/icon/translate.svg" type="image/svg+xml" /> -->
+            <SvgIcon name="translate" color="white" />
+        </el-header>
+        <div style="height: 100%">
+            <el-main> <slot name="main"></slot></el-main>
+            <!-- <el-footer> </el-footer> -->
+        </div>
+    </el-container>
+</template>
+
+<script lang="ts">
+import SvgIcon from "../SvgIcon.vue";
+// import Translate from "../../assets/icons/translate.svg";
+
+export default { components: { SvgIcon } };
+</script>
+
+<style scoped>
+#title {
+    font-size: 24px;
+    text-decoration: none;
+    color: white;
+    margin: 0;
+}
+.el-header {
+    height: 50px;
+    background-color: #409eff;
+
+    display: flex;
+    align-items: center;
+    padding-left: 20px;
+    justify-content: space-between;
+}
+.el-main {
+    height: 100%;
+    padding: 0;
+}
+</style>
---
+++ src/views/Demo.vue
@@ -0,0 +1,175 @@
+<template>
+    <BasicLayout>
+        <template #main>
+            <div id="main">
+                <div
+                    id="operator-wrap"
+                    class="wrap"
+                    style="padding-right: 15px; width: 50%"
+                >
+                    <!-- 生成操作区域 -->
+                    <div
+                        style="
+                            display: flex;
+                            justify-content: space-between;
+                            align-items: center;
+                            margin: 5px 0 15px 0;
+                        "
+                    >
+                        <h1>AI动作生成</h1>
+                        <el-radio-group v-model="needTranslate">
+                            <el-radio-button label="true">中</el-radio-button>
+                            <el-radio-button label="false">En</el-radio-button>
+                        </el-radio-group>
+                    </div>
+
+                    <el-input
+                        style="padding-bottom: 15px"
+                        placeholder="请输入提示词以生成动作..."
+                        rows="6"
+                        resize="none"
+                        type="textarea"
+                    ></el-input>
+                    <div style="text-align: right">
+                        <el-button> 清除 </el-button>
+                        <el-button type="primary">立即生成</el-button>
+                    </div>
+
+                    <el-divider></el-divider>
+
+                    <!-- 参考示例显示 -->
+                    <h2>参考示例</h2>
+                    <ul style="padding-left: 15px">
+                        <li v-for="example in examples">{{ example.zh }}</li>
+                    </ul>
+                    <p style="color: #409eff">
+                        目前可支持地武器动作模态类型：
+                        匕首/大剑/太刀/曲剑/斧/载/锤/拳头
+                    </p>
+                </div>
+
+                <div style="width: 20px"></div>
+                <div id="right-wrap" style="width: 50%">
+                    <div class="wrap" style="height: 90%"></div>
+                    <div
+                        class="wrap"
+                        style="
+                            height: 8%;
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                        "
+                    >
+                        <el-button class="my-btn">
+                            <el-icon><Download /></el-icon> <span>mp4下载</span>
+                        </el-button>
+                        <el-button class="my-btn">
+                            <el-icon><Download /></el-icon> <span>bvh下载</span>
+                        </el-button>
+                    </div>
+                </div>
+                <!-- header -->
+            </div>
+        </template>
+    </BasicLayout>
+</template>
+
+<script lang="ts">
+import { Download } from "@element-plus/icons-vue";
+
+import BasicLayout from "../components/layout/Basic.vue";
+
+export default {
+    components: { BasicLayout, Download },
+    data() {
+        return {
+            needTranslate: false,
+            examples: [
+                {
+                    zh: "角色双手自然下垂与身体两侧，身体中心稍微下压，向左缓慢移动",
+                },
+                { zh: "角色轻盈向前跑动" },
+                { zh: "角色轻盈地向前方翻滚，快速地起身" },
+                {
+                    zh: "角色受击，身体大硬直，均速起身，惊慌失措地身体失衡向左退，右手挥舞在空中",
+                },
+                {
+                    zh: "角色右手持大剑，蓄力重击。角色双腿半蹲，上半身微微向右侧侧身，紧接着向前劈砍。最后回到原始站立姿势。",
+                },
+                {
+                    zh: "角色双持戟进行蓄力重击，扎实地侧身扭腰向左，右腿向前迈步，双手共握武器，从左后蓄力，然后向前连续挥两圈。",
+                },
+                {
+                    zh: "角色左手持刺剑进行轻击，轻盈地右手挽一个刀花后向前刺击，同时向前碎步，随后恢复站姿。",
+                },
+                {
+                    zh: "角色向右转身，扎马步，右手将模丸插入左手刀鞘中蓄力，然后顺时针转身向前挥。",
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style>
+body {
+    /* overflow-y: hidden; */
+}
+
+#main {
+    background-color: #f7f7f7;
+    height: 100%;
+    padding: 25px 50px;
+    box-sizing: border-box;
+
+    overflow-y: hidden;
+
+    display: flex;
+    /* flex-direction: row; */
+    justify-content: space-between;
+}
+
+.wrap {
+    /* flex: 1; */
+
+    background-color: white;
+    border: 1px gray;
+    padding: 20px 25px;
+    box-sizing: border-box;
+    /* width: 49%; */
+    /* height: 100%; */
+
+    border-radius: 15px;
+    border: 1.5px solid #cfcfcf;
+}
+#operator-wrap {
+    h1 {
+        color: #409eff;
+        font-size: 32px;
+    }
+    h2 {
+        color: #409eff;
+        font-size: 20px;
+    }
+}
+#right-wrap {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+/* .set-other-btn {
+    color: #fff;
+    background-color: #fa4eab;
+    border-color: #fa4eab;
+}
+.set-other-btn:hover {
+    background-color: #fe83c6;
+    border-color: #fe83c6;
+}
+
+.set-other-btn:focus {
+    background-color: #fe83c6;
+    border-color: #fe83c6;
+} */
+</style>
