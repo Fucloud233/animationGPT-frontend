cbb12bfb54cc8666f120758c11515569d21a15f6
Fucloud-arch <270928474@qq.com>
2024-02-25 22:13:22 +0800

    feat: SubTitle style for introduce

---
+++ src/components/SubTitle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p id="top">{{ top }}</p>
+        <p id="bottom">{{ bottom }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        top: String,
+        bottom: String,
+    },
+};
+</script>
+
+<style scoped>
+#top {
+    font-size: 22px;
+    margin-bottom: 5px;
+    color: #409eff;
+}
+#bottom {
+    font-size: 26px;
+    margin-top: 0;
+    font-weight: bold;
+}
+</style>
