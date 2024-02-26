848fc92b68821a9a1e85158de8bbb39c15a76859
Fucloud-arch <270928474@qq.com>
2024-02-26 14:03:04 +0800

    fix: hide /#/

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 var routes = [
     {
@@ -14,7 +14,7 @@ var routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: routes,
 });
 
