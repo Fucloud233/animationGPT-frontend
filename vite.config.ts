5de04f5dbb946cfbcfe22c267ac42eb77896ddd3
Fucloud-arch <270928474@qq.com>
2024-03-08 20:40:43 +0800

    config; proxy for backend

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,12 @@ export default defineConfig({
     plugins: [vue(), svgLoader()],
     server: {
         port: 8080,
+        proxy: {
+            "/api": {
+                target: "http://127.0.0.1:8082/",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
     },
 });
