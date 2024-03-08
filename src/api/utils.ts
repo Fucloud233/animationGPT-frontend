fd520a1d40097c94f5bfadfb42d2298b71a9b1e7
Fucloud-arch <270928474@qq.com>
2024-03-08 20:41:23 +0800

    upload: basic request utils

---
+++ src/api/utils.ts
@@ -0,0 +1,56 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export function postRequest(apiUrl: string, data: any): AxiosRequestConfig {
+    return {
+        method: "post",
+        url: apiUrl,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        data: data,
+    };
+}
+
+export function deleteRequest(apiUrl: string, data: any) {
+    return axios({
+        method: "delete",
+        url: apiUrl,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        data: data,
+    });
+}
+
+export function putRequest(apiUrl: string, data: any) {
+    return axios({
+        method: "put",
+        url: apiUrl,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        data: data,
+    });
+}
+
+export function getRequest(apiUrl: string, params: any) {
+    return axios({
+        method: "get",
+        url: apiUrl,
+        params: params,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
+
+export function getApiUrl(name: string) {
+    var apiUrl = "/api/" + name;
+    return apiUrl;
+}
+
+export function blob2Url(data: any, type: string) {
+    const url = window.URL.createObjectURL(new Blob([data], { type: type }));
+
+    return window.URL.createObjectURL(new Blob([data], { type: type }));
+}
