3d031676c822f82177a962b6b797c334dc8c36c5
Fucloud-arch <270928474@qq.com>
2024-03-23 11:08:04 +0800

    feat: rename the filename of examples

---
+++ script/rename.py
@@ -0,0 +1,86 @@
+from pprint import pprint
+from pathlib import Path
+import json
+
+import pandas as pd
+
+
+filename = "./introduce_examples.csv"
+
+lang_list = ["cn", "en"]
+
+
+# 给测试用例重命名
+def rename():
+
+    data = pd.read_csv(filename).to_dict()
+
+    names = [name for name in data['ID'].values()]
+
+    kinds = [
+        # "OriginalSMPL",
+        # "Original", 
+        # "S4-50", 
+        # "S4-70"
+        "agpt",
+        "agpt_plus",
+        "mdn",
+        "mgpt",
+        "mld"
+        ]
+
+    format = "gif"
+
+    reverse = True
+
+    for kind in kinds:
+        for (i, name) in enumerate(names):
+            folder_name = f"./gif/{kind}/"
+
+            # 特殊处理
+            if kind == "Original":
+                name = name + "_original"
+            elif kind == "OriginalSMPL":
+                name = name + "_original_mesh"
+                
+            index_name = "%02d.%s"%(i, format)
+            id_name =  f"{name}.{format}"
+                
+            try:
+                if reverse:
+                    path = Path(folder_name + index_name)
+                    path.rename(folder_name + id_name)
+                else: 
+                    path = Path(folder_name + id_name)
+                    path.rename(folder_name + index_name)
+            except:
+                pass
+
+            
+
+# 将 csv 文件转换为 json 文件
+def extract():
+    data = pd.read_csv(filename).to_dict()
+
+    for lang in lang_list:
+
+        text_cn = [name for name in data[f'text_{lang}'].values()]
+
+        with open(f"{lang}.json", 'w') as f:
+            json.dump(text_cn, f, indent=4, ensure_ascii=False)
+
+
+def extract_examples():
+    data = pd.read_csv(filename).to_dict()
+
+    for lang in lang_list:
+        text = [ name for name in data[f'{lang}'].values()]
+
+        with open(f"{lang}_example.json", 'w') as f:
+            json.dump(text, f, indent=4, ensure_ascii=False)
+
+if __name__ == "__main__":
+
+    # extract_examples()
+    rename()
+
