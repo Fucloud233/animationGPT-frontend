d31a81941003c1dfd61d9f971eb91eb8607bdc8a
Fucloud <fucloud@mac-mini-nlp.local>
2024-04-05 18:56:46 +0800

    feat: remove article entrance

--- src/views/Introduce.vue
+++ src/views/Introduce.vue
@@ -3,65 +3,41 @@
         <div class="cover">{{ $t("introduce.coverTitle") }}</div>
         <div class="title">{{ $t("introduce.mainTitle") }}</div>
         <div class="jump">
-            <el-button
-                class="my-btn"
-                style="box-shadow: 0 0 5px"
-                v-for="router in routers"
-                @click="router.action"
-            >
-                <SvgIcon
-                    :name="router.name"
-                    color="#409eff"
-                    :size="18"
-                ></SvgIcon
+            <el-button class="my-btn" style="box-shadow: 0 0 5px" v-for="router in routers" @click="router.action">
+                <SvgIcon :name="router.name" color="#409eff" :size="18"></SvgIcon
                 ><span>{{ router.label }}</span></el-button
             >
         </div>
-        <p style="margin: 30px 0 40px 0; position: relative">
-            Yihao Liao, Yiyu Fu, Ziming Cheng, Jiangfeiyang Wang
-        </p>
+        <p style="margin: 30px 0 40px 0; position: relative">Yihao Liao, Yiyu Fu, Ziming Cheng, Jiangfeiyang Wang</p>
     </div>
 
     <div id="body-area">
-        <SubTitle
-            :top="$t('introduce.article')"
-            :bottom="$t('introduce.abstract')"
-        />
+        <SubTitle :top="$t('introduce.article')" :bottom="$t('introduce.abstract')" />
         <p>
-            Though the advancement of pre-trained large language models unfolds,
-            the exploration of building a unified model for language and other
-            multimodal data, such as motion, remains challenging and untouched
-            so far. Fortunately, human motion displays a semantic coupling akin
-            to human language, often perceived as a form of body language. By
-            fusing language data with large-scale motion models, motion-language
-            pre-training that can enhance the performance of motion-related
-            tasks becomes feasible. Driven by this insight, we propose
-            MotionGPT, a unified, versatile, and user-friendly motion-language
-            model to handle multiple motion-relevant tasks. Specifically, we
-            employ the discrete vector quantization for human motionand transfer
-            3D motion into motion tokens, similar to the generation process
-            ofword tokens. Building upon this “motion vocabulary”, we perform
-            language modeling on both motion and text in a unified manner,
-            treating human motion as a specific language. Moreover, inspired by
-            prompt learning, we pre-train MotionGPT with a mixture of
-            motion-language data and fine-tune it on prompt-based
-            question-and-answer tasks. Extensive experiments demonstrate that
-            MotionGPT achieves state-of-the-art performances on multiple motion
-            tasks including text-driven motion generation, motion captioning,
-            motion prediction, and motion in-between.
+            Though the advancement of pre-trained large language models unfolds, the exploration of building a unified
+            model for language and other multimodal data, such as motion, remains challenging and untouched so far.
+            Fortunately, human motion displays a semantic coupling akin to human language, often perceived as a form of
+            body language. By fusing language data with large-scale motion models, motion-language pre-training that can
+            enhance the performance of motion-related tasks becomes feasible. Driven by this insight, we propose
+            MotionGPT, a unified, versatile, and user-friendly motion-language model to handle multiple motion-relevant
+            tasks. Specifically, we employ the discrete vector quantization for human motionand transfer 3D motion into
+            motion tokens, similar to the generation process ofword tokens. Building upon this “motion vocabulary”, we
+            perform language modeling on both motion and text in a unified manner, treating human motion as a specific
+            language. Moreover, inspired by prompt learning, we pre-train MotionGPT with a mixture of motion-language
+            data and fine-tune it on prompt-based question-and-answer tasks. Extensive experiments demonstrate that
+            MotionGPT achieves state-of-the-art performances on multiple motion tasks including text-driven motion
+            generation, motion captioning, motion prediction, and motion in-between.
         </p>
 
         <ExampleDisplay />
 
         <SubTitle :top="$t('introduce.our')" :bottom="$t('introduce.method')" />
         <p>
-            To involve large language data and models in the motion generation
-            tasks, we propose a unified motion-language framework named
-            MotionGPT. MotionGPT consists of a motion tokenizer responsible for
-            converting raw motion data into discrete motion tokens, as well as a
-            motion-aware language model that learns to understand the motion
-            tokens from large language pre-training models by corresponding
-            textual descriptions.
+            To involve large language data and models in the motion generation tasks, we propose a unified
+            motion-language framework named MotionGPT. MotionGPT consists of a motion tokenizer responsible for
+            converting raw motion data into discrete motion tokens, as well as a motion-aware language model that learns
+            to understand the motion tokens from large language pre-training models by corresponding textual
+            descriptions.
         </p>
         <el-image :src="methodUrl"></el-image>
 
@@ -82,8 +58,7 @@ export default {
     },
     data() {
         return {
-            methodUrl: new URL(`../assets/test/method.png`, import.meta.url)
-                .href,
+            methodUrl: new URL(`../assets/test/method.png`, import.meta.url).href,
             routers: [
                 {
                     name: "app",
@@ -92,19 +67,18 @@ export default {
                         this.$router.push({ name: "demo" });
                     },
                 },
-                {
-                    name: "article",
-                    label: "Article",
-                    action: () => {
-                        window.location.href = "https://www.zhihu.com/";
-                    },
-                },
+                // {
+                //     name: "article",
+                //     label: "Article",
+                //     action: () => {
+                //         window.location.href = "https://www.zhihu.com/";
+                //     },
+                // },
                 {
                     name: "github",
                     label: "Github",
                     action: () => {
-                        window.location.href =
-                            "https://github.com/fyyakaxyy/animationGPT";
+                        window.location.href = "https://github.com/fyyakaxyy/animationGPT";
                     },
                 },
             ],
