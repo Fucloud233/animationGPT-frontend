fe10fa3166a3a201c1b2721c100e45b87d57814c
fucloud <fucloud@Fuclouds-MacBook-Air.local>
2024-04-12 14:10:43 +0800

    import: about markdown

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -21,3 +21,15 @@ declare interface ExamplesConfig {
 declare interface Window {
     config: Config;
 }
+
+declare module '*.vue' {
+    import type { ComponentOptions } from 'vue'
+    const Component: ComponentOptions
+    export default Component
+}
+  
+declare module '*.md' {
+    import type { ComponentOptions } from 'vue'
+    const Component: ComponentOptions
+    export default Component
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,23 @@ import vue from "@vitejs/plugin-vue";
 
 import svgLoader from "vite-svg-loader";
 
+import Markdown from "vite-plugin-md";
+
+import FootNote from "markdown-it-footnote";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), svgLoader()],
+    plugins: [
+        vue({
+            include: [/\.vue$/, /.md$/],
+        }),
+        svgLoader(),
+        Markdown({
+            markdownItSetup(md) {
+                md.use(FootNote);
+            },
+        }),
+    ],
     server: {
         port: 8080,
         proxy: {
