c8219b287b0292333bffcd5639e49c4054e650d3
fucloud-macbook <270928474@qq.com>
2024-04-16 19:51:59 +0800

    feat encupsulate sub article block

---
+++ src/components/introduce/SubBlock.vue
@@ -0,0 +1,25 @@
+<template>
+    <SubTitle :top="$t(`introduce.subTitle.${topTitle}`)" :bottom="$t(`introduce.subTitle.${mainTitle}`)" />
+    <p>{{ $t(`introduce.content.${mainTitle}`) }}</p>
+    <slot name="tail"></slot>
+</template>
+
+<script lang="ts">
+import SubTitle from "./SubTitle.vue";
+
+export default {
+    props: {
+        mainTitle: {
+            type: String,
+            required: true,
+        },
+        topTitle: {
+            type: String,
+            required: true,
+        },
+    },
+    components: {
+        SubTitle,
+    },
+};
+</script>
